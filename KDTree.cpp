8ad41433b10fe6f374a2da5dd759dbdeb377f89f
Sivaram Ambikasaran <siva.1985@gmail.com>
2013-12-03 01:56:29 -0500

    Test

--- KDTree.cpp
+++ KDTree.cpp
@@ -7,3 +7,46 @@
 //
 
 #include "KDTree.hpp"
+
+KDTree::KDTree(const unsigned n_Locations, const unsigned n_Dimension, double** locations) {
+        this->n_Locations       =       n_Locations;
+        this->n_Dimension       =       n_Dimension;
+        this->locations         =       new double* [n_Locations];
+        for (unsigned j=0; j<n_Locations; ++j) {
+                this->locations[j]      =       new double[n_Dimension];
+                for (unsigned k=0; k<n_Dimension; ++k) {
+                        this->locations[j][k]   =       locations[j][k];
+                }
+        }
+}
+
+void KDTree::sort_KDTree() {
+        sorted_Locations        =       new double* [n_Locations];
+        for (unsigned j=0; j<n_Locations; ++j) {
+                sorted_Locations[j]    =       new double[n_Dimension];
+                for (unsigned k=0; k<n_Dimension; ++k) {
+                        sorted_Locations[j][k] =       locations[j][k];
+                }
+        }
+}
+
+void KDTree::get_Location(double** locations) {
+        locations       =       new double* [n_Locations];
+        for (unsigned j=0; j<n_Locations; ++j) {
+                locations[j]    =       new double[n_Dimension];
+                for (unsigned k=0; k<n_Dimension; ++k) {
+                        locations[j][k] =       sorted_Locations[j][k];
+                }
+        }
+}
+
+void KDTree::get_Location(const unsigned n_Index, double* location) {
+        location        =       new double [n_Dimension];
+        for (unsigned k=0; k<n_Dimension; ++k) {
+                location[k]     =       sorted_Locations[n_Index][k];
+        }
+}
+
+void KDTree::get_Location(const unsigned n_Index, const unsigned n_Coordinate, double& location) {
+        location        =       sorted_Locations[n_Index][n_Coordinate];
+}
--- KDTree.hpp
+++ KDTree.hpp
@@ -22,7 +22,7 @@ public:
         //      Constructor.
         //      Usage:
         //      KDTree(10000, 3, locations);
-        KDTree(const unsigned n_Locations, const unsigned n_Dimension, const double** locations);
+        KDTree(const unsigned n_Locations, const unsigned n_Dimension, double** locations);
         
         //      KDTree sorting.
         void sort_KDTree();
@@ -30,7 +30,7 @@ public:
         //      Obtains all the sorted location.
         //      Usage:
         //      get_Location(location);
-        void get_Location(double** location);
+        void get_Location(double** locations);
         
         //      Given an index, obtains the location from the sorted location.
         //      n_Index <= n_Locations.
@@ -42,6 +42,6 @@ public:
         //      n_Index <= n_Locations and n_Coordinate <= n_Dimension
         //      Usage:
         //      get_Location(1234, 2, location);
-        void get_Location(const unsigned n_Index, const unsigned n_Coordinate, double location);
+        void get_Location(const unsigned n_Index, const unsigned n_Coordinate, double& location);
 };
 #endif /* defined(__KDTREE_HPP__) */
