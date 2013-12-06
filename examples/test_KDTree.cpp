fae580d6e661bd1a635a69e7b475c1cdc37b30cf
Sivaram Ambikasaran <siva.1985@gmail.com>
2013-12-05 23:09:50 -0500

    Changes to example file

--- examples/test_KDTree.cpp
+++ examples/test_KDTree.cpp
@@ -12,10 +12,11 @@
 
 int main(int argc, char** argv) {
         assert(argc==3);
-        unsigned n_Locations    =       atoi(argv[1]);
-        unsigned n_Dimension    =       atoi(argv[2]);
-        double* locations;
+        unsigned n_Locations    =       atoi(argv[1]);  //      Number of points.
+        unsigned n_Dimension    =       atoi(argv[2]);  //      Dimension.
+        double* locations;                              //      Stores all the locations.
         locations               =       new double [n_Locations*n_Dimension];
+        //      Generates random locations.
         unsigned count          =       0;
         for (unsigned j=0; j<n_Locations; ++j) {
                 for (unsigned k=0; k<n_Dimension; ++k) {
@@ -24,6 +25,7 @@ int main(int argc, char** argv) {
                 }
         }
         std::cout << std::scientific;
+        //      Displays the random locations to begin with.
         for (unsigned j=0; j < n_Locations; ++j) {
                 for (unsigned k=0; k < n_Dimension; ++k) {
                         std::cout << locations[j*n_Dimension+k] << "\t";
@@ -31,15 +33,19 @@ int main(int argc, char** argv) {
                 std::cout << std::endl;
         }
 
+        //      Creates a KDTree given the locations.
         KDTree* A               =       new KDTree(n_Locations, n_Dimension, locations);
         delete locations;
 
         double* sorted_Locations=       new double[n_Locations*n_Dimension];
         
-
+        //      Sorts the locations based on KDTree.
         A->sort_KDTree();
 
+        //      Obtains the sorted location.
         A->get_Location(sorted_Locations);
+
+        //      Displays the sorted locations.
         std::cout << "Sorted locations are: " << std::endl;
         for (unsigned j=0; j < n_Locations; ++j) {
                 for (unsigned k=0; k < n_Dimension; ++k) {
